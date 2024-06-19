8e0a3703d191e10e662b1f335cbcbb18e2ad9dbc
Jefferson Maia <jeffersonmaia.dev@gmail.com>
2024-06-18 21:16:20 -0700

    commit init

---
+++ src/test/java/com/example/msdesafiotecnico/MsDesafioTecnicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msdesafiotecnico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsDesafioTecnicoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
